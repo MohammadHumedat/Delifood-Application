94b60f8506729c690643bf0b89e9c00f2cbc46a7
Mohammad Humedat <humedat23@gmail.com>
2024-09-12 22:20:49 +0300

    add the favorite item and creat cards for each item

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/models/food_item.dart';
+import 'package:food_delivery/widget/favorite_item.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding:  EdgeInsets.all(8),
-      child: Center(
+    return Padding(
+      padding: const EdgeInsets.all(17.0),
+      child: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [Text('Favorite  Page')],
+          children:
+              food.map((foodItem) => FavoriteItem(foodItem: foodItem)).toList(),
         ),
       ),
     );
