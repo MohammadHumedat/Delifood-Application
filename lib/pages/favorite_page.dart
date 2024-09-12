959338e6fbebdadcb93f9c123eb2be884f445851
Mohammad Humedat <humedat23@gmail.com>
2024-09-12 23:14:23 +0300

    add List view

--- lib/pages/favorite_page.dart
+++ lib/pages/favorite_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/models/food_item.dart';
 import 'package:food_delivery/widget/favorite_item.dart';
+import 'package:food_delivery/widget/food_grid_item.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({super.key});
@@ -16,5 +17,17 @@ class FavoritePage extends StatelessWidget {
         ),
       ),
     );
+    // Padding(
+    //   padding: const EdgeInsets.all(8.0),
+    //   child: ListView.builder(
+    //       itemCount: food.length,
+    //       itemBuilder: (BuildContext context, int index) {
+    //         return FoodGridItem(
+    //             foodItem: FoodItem(
+    //                 name: food[index].name,
+    //                 imgurl: food[index].imgurl,
+    //                 price: food[index].price));
+    //       }),
+    // );
   }
 }
