3a583e33ebf862f6302c71c0dc306bb824667319
Mohammad hmedat <humedat23@gmail.com>
2025-07-10 19:52:23 +0300

    feat : Add APPAssets class model to store the all paths of images that uses many times to perform best practicing in App Archetechure

---
+++ lib/utilities/app_assets.dart
@@ -0,0 +1,14 @@
+class AppAssets {
+  // Home page
+  static const String burgerIcon = 'assets/images/burger.png'; // Burger Icon
+  static const String pizzaIcon = 'assets/images/pizza.png'; // Pizza Icon
+  static const String pastaIcon = 'assets/images/spaguetti.png'; // Pasta Icon
+  static const String burgerBanner =
+      'assets/images/classic_burger.jpg'; // Classic burger in banner of home page
+
+  // Account Page
+  static const String profilePhoto = 'assets/images/Myphoto.jpg';
+
+  // General
+  static const String emptyState = 'assets/images/empty_state.png';
+}
